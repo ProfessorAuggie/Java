9fb79547514f7ba0bd810c7a4f0db5f823ac9660
Vaibhav Kushwaha <93552725+ProfessorAuggie@users.noreply.github.com>
2025-01-23 08:56:46 +0530

    Create File.m

---
+++ File.m
@@ -0,0 +1,53 @@
+Enter the number of employees: 2
+
+--- Employee Management System ---
+1. Add Employee
+2. Show All Employees
+3. Search Employee
+4. Update Employee
+5. Delete Employee
+6. Exit
+Choose an option: 1
+
+Enter details for new employee:
+Employee ID: 3001
+Employee Name: Ramesh Iyer
+Department: Operations
+Designation Code (e: Engineer, c: Consultant, k: Clerk, r: Receptionist, m: Manager): r
+Basic Salary: 30000
+HRA: 6000
+Income Tax (IT): 2500
+Employee added successfully.
+
+--- Employee Management System ---
+1. Add Employee
+2. Show All Employees
+3. Search Employee
+4. Update Employee
+5. Delete Employee
+6. Exit
+Choose an option: 1
+
+Enter details for new employee:
+Employee ID: 3002
+Employee Name: Rekha Rao
+Department: Admin
+Designation Code (e: Engineer, c: Consultant, k: Clerk, r: Receptionist, m: Manager): e
+Basic Salary: 55000
+HRA: 11000
+Income Tax (IT): 4000
+Employee added successfully.
+
+--- Employee Management System ---
+1. Add Employee
+2. Show All Employees
+3. Search Employee
+4. Update Employee
+5. Delete Employee
+6. Exit
+Choose an option: 2
+
+Emp No. Emp Name         Department     Designation     Basic Salary  HRA         IT        Total Salary DA
+----------------------------------------------------------------------------------------------------------------
+3001      Ramesh Iyer     Operations     Receptionist    30000.00     6000.00     2500.00    34000.00      15000.00
+3002      Rekha Rao       Admin          Engineer        55000.00     11000.00    4000.00    67000.00      20000.00
